8052bee47c79ea948a67e22793fd6838ba562a15
LeahBargnesi <leahbarg@buffalo.edu>
2022-09-21 20:31:59 -0400

    third commit

--- week_03/case_study_03.R
+++ week_03/case_study_03.R
@@ -1,7 +1,7 @@
 library(ggplot2); library(gapminder); library(dplyr) 
 gapminder
 filter_gapminder <- filter(gapminder,country!="Kuwait")
-ggplot(filter_gapminder,aes(x= lifeExp, y= gdpPercap))+
+plot1<- ggplot(filter_gapminder,aes(x= lifeExp, y= gdpPercap))+
   geom_point() + facet_wrap(~year,nrow=1) +
   scale_y_continuous(trans = "sqrt") + theme_bw() +
   labs(title="Wealth and Life Expectancy Through Time", x="Life Expectancy", y="GDP Per Capita")
@@ -9,9 +9,13 @@ group_by(filter_gapminder, continent, year)
 gapminder_continent <- summarize(filter_gapminder, gdpPercapweighted = weighted.mean(x = gdpPercap, w = pop),
          pop = sum(as.numeric(pop)))
 gapminder_continent
-ggplot(filter_gapminder, aes(x= year, y= gdpPercap))+
+plot2<- ggplot(gapminder_continent,aes(x= year,y=gdpPercap))+
   geom_line()+ geom_point()+
   geom_line(data=gapminder_continent, color="black")+
   geom_point(data=gapminder_continent,show.legend = TRUE )+
-  facet_wrap(~year,nrow=1)+ theme_bw()+
+  facet_wrap(~continent,nrow=1)+ theme_bw()+
   labs(title="Wealth and Life Expectancy Through Time", x="Year", y="GDP per Capita")
+png(filename="plot1.png", width= 15, height=15, units="in", res=72)
+png(filename="plot2.png", width=15, height=15, units="in", res=72)
+plot1
+plot2
